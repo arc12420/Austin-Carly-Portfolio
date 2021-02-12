26d05dae72f01beb756c1e6083b0d33dc3c3ac51
Austin Carly <austinrcarly16@gmail.com>
2021-02-12 16:42:46 -0700

    ProjectsPageBasicLayout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,50 @@ import "./Projects.css";
 function Projects() {
   return (
     <main className="projectsComponent">
-      <div className="mainBox">Projects Page</div>
+      <div className="mainBox">
+        <div className="projects">
+        <section className="authorKortneyKeisel">
+          <div className="projectDescription">
+            
+          Kort
+          </div>
+          <div className="projectImage">
+
+          Kort
+          </div>
+          </section>
+        <section className="theIvoryFoundation">
+          <div className="projectImage">
+            
+          Ivory
+          </div>
+          <div className="projectDescription">
+            
+          Ivory
+          </div>
+          </section>
+        <section className="theBrood">
+          <div className="projectDescription">
+            
+          Brood
+          </div>
+          <div className="projectImage">
+            
+          Brood
+          </div>
+          </section>
+        <section className="andersonAuctionHouse">
+          <div className="projectImage">
+            
+          Auction
+          </div>
+          <div className="projectDescription">
+            
+          Auction
+          </div>
+          </section>
+      </div>
+      </div>
     </main>
   );
 }
